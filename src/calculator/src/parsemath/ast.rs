5ef607e36bd44afe219f3be5563146af3233c8d2
Roman Zanko <roman.zanko.vl@gmail.com>
2024-09-09 19:22:43 +0400

    Added evaluator to calculator

--- src/calculator/src/parsemath/ast.rs
+++ src/calculator/src/parsemath/ast.rs
@@ -1,3 +1,5 @@
+use std::error;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Node {
     Add(Box<Node>, Box<Node>),
@@ -7,4 +9,16 @@ pub enum Node {
     Caret(Box<Node>, Box<Node>),
     Negative(Box<Node>),
     Number(f64),
+}
+
+pub fn eval(node: Node) -> Result<f64, Box<dyn error::Error>> {
+    match node {
+        Node::Number(n) => Ok(n),
+        Node::Add(lhs, rhs) => Ok(eval(*lhs)? + eval(*rhs)?),
+        Node::Subtract(lhs, rhs) => Ok(eval(*lhs)? - eval(*rhs)?),
+        Node::Multiply(lhs, rhs) => Ok(eval(*lhs)? * eval(*rhs)?),
+        Node::Divide(lhs, rhs) => Ok(eval(*lhs)? / eval(*rhs)?),
+        Node::Caret(lhs, rhs) => Ok(eval(*lhs)?.powf(eval(*rhs)?)),
+        Node::Negative(expr) => Ok(-eval(*expr)?),
+    }
 }
